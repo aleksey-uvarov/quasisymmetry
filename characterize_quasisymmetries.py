35bda70b2827695acdb11366e16ba2109e354e6b
Alexey Uvarov <alexejuv@gmail.com>
2026-04-23 13:12:16 -0400

    add linprog for diagonal part

--- characterize_quasisymmetries.py
+++ characterize_quasisymmetries.py
@@ -13,6 +13,7 @@ import openfermion as of
 from itertools import combinations, product
 from typing import Tuple
 from copy import deepcopy
+from scipy.optimize import linprog
 
 from chemistry import get_mol, build_U_from_thetas
 
@@ -21,7 +22,7 @@ def diagonal_energies(h_in: of.InteractionOperator,
                       U: np.ndarray) -> Tuple[float, float, float, float]:
     """Rotate h into the basis specified by U and return
     effective energies of its diagonal part:
-    loweest energy;
+    lowest energy;
     electron removal energy (HOMO);
     electron addition energy (LUMO);
     electron excitation energy (HOMO -> LUMO)"""
@@ -37,9 +38,55 @@ def diagonal_energies(h_in: of.InteractionOperator,
     for i, j in product(range(len(one_body_coeffs)), repeat=2):
         two_body_coeffs[i, j] = h.two_body_tensor[i, j, j, i] - h.two_body_tensor[i, j, i, j]
 
+    e_lowest, ns_lowest = solve_qubo(one_body_coeffs, two_body_coeffs)
+    print(e_lowest, ns_lowest)
 
-    return None, None, None, None
+    return e_lowest, None, None, None
 
+
+def solve_qubo(linear_term: np.ndarray,
+               quadratic_term: np.ndarray) -> Tuple[float, np.ndarray]:
+    """Given a QUBO problem, return the optimal value and the optimal x.
+    We first convert the problem to a constrained integer linear programming problem,
+    then solve it with linprog"""
+
+    linear_effective = linear_term + np.diag(quadratic_term)
+    upper_triangular = np.triu(quadratic_term + quadratic_term.T, 1)
+    mask = np.ones_like(upper_triangular)
+    mask = np.triu(mask, 1)
+    nz = np.nonzero(mask)
+    nz_flat = np.array([upper_triangular[nz[0][i], nz[1][i]]
+                        for i in range(len(nz[0]))])
+    cost_vector = np.concatenate((linear_effective, nz_flat))
+
+    constraints = []
+    for z_count in range(len(nz[0])):
+        i = nz[0][z_count]
+        j = nz[1][z_count]
+
+        row_lt_i = np.zeros_like(cost_vector)
+        row_lt_i[i] = -1
+        row_lt_i[len(linear_term) + z_count] = 1
+
+        row_lt_j = np.zeros_like(cost_vector)
+        row_lt_j[j] = -1
+        row_lt_j[len(linear_term) + z_count] = 1
+
+        row_gt_sum = np.zeros_like(cost_vector)
+        row_gt_sum[i] = 1
+        row_gt_sum[j] = 1
+        row_gt_sum[len(linear_term) + z_count] = -1
+
+        constraints.append(row_lt_i)
+        constraints.append(row_lt_j)
+        constraints.append(row_gt_sum)
+
+    A_ub = np.array(constraints)
+    b_ub = np.array([0, 0, 1] * len(nz[0]))
+
+    res = linprog(cost_vector, A_ub, b_ub, bounds=(0, 1), integrality=1)
+
+    return res.fun, res.x[:len(linear_term)]
 
 
 if __name__=="__main__":
