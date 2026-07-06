77415003e0ec6b383a82b891fb9a5f390b7ac694
dimdamdum <damiano.aliverti@physik.uni-muenchen.de>
2026-07-06 19:56:55 -0400

    implemented eval equation-based score

--- dam/cost_functions.py
+++ dam/cost_functions.py
@@ -32,6 +32,70 @@ def commutator_cost_v2(moldata: ffsim.MolecularData,
         return total_nc
     return f
 
+def eval_eq_cost(symmetries: list, evals: list,
+                    reference_state: np.ndarray, norb:int, nelec:int) -> Callable:
+    if len(symmetries) != len(evals):
+        raise ValueError("len(evals) must match len(symmetries)")
+    def f(x):
+        U = x_to_rotation(x, norb)
+        rotated_state = ffsim.apply_orbital_rotation(reference_state,
+                                                     U,
+                                                     norb,
+                                                     nelec)
+        total = 0
+        for i in range(len(symmetries)):
+            total += np.linalg.norm(symmetries[i] @ rotated_state - evals[i] * rotated_state)**2
+        return total
+    return f
+
+# compare with show_symmetries.py, lines 44-62
+def get_heatmap_data_nc_score(h, ref_state, norb, diag_operators, off_diag_operators, upscale_factor=1):
+    """Returns matrix of nc_scores, with entries ||[H, quasisymmetry_operator]|Ψ>||²"""
+    # Initialize heatmap data
+    nc_scores = np.zeros((norb, norb))
+
+    # precompute H|psi>
+    h_on_ref_state = h @ ref_state
+
+    # Diagonal: Possibility to scale up with upscale_factor, for better visualization
+    for i in range(norb):
+        term1 = h @ (diag_operators[i] @ ref_state)
+        term2 = diag_operators[i] @ h_on_ref_state # using the wrapper variable type. Commutator has same/related type
+        commutator_on_state = term1 - term2
+        nc_scores[i, i] = upscale_factor * np.linalg.norm(commutator_on_state)**2
+
+    # Off-diagonal
+    # Need to arefully handle indices (e.g., do inverse of index mapping of what is done in cluster_number.build_two_orb_num_operators)
+    for i in range(norb):
+        for j in range(i+1, norb):
+            flat_index = i * norb - i * (i + 1) // 2 + j - i - 1 # checked
+            op_ij = off_diag_operators[flat_index]
+            term1 = h @ (op_ij @ ref_state)
+            term2 = op_ij @ h_on_ref_state
+            commutator_on_state = term1 - term2
+            nc_scores[i, j] = np.linalg.norm(commutator_on_state)**2
+
+    return nc_scores
+
+def get_heatmap_data_eval_eq_score(ref_state, norb, diag_operators, off_diag_operators, diag_evals, off_diag_evals, upscale_factor=1):
+    """Returns matrix of eigenvalue equation-based scores, with entries ||(quasisymmetry operator - eval * I)|Ψ>||²"""
+    # Initialize heatmap data
+    eval_scores = np.zeros((norb, norb))
+
+    # Diagonal: Possibility to scale up with upscale_factor, for better visualization
+    for i in range(norb):
+        eval_scores[i, i] = upscale_factor * np.linalg.norm(diag_operators[i] @ ref_state - diag_evals[i] * ref_state)**2
+
+    # Off-diagonal
+    # Need to arefully handle indices (e.g., do inverse of index mapping of what is done in cluster_number.build_two_orb_num_operators)
+    for i in range(norb):
+        for j in range(i+1, norb):
+            flat_index = i * norb - i * (i + 1) // 2 + j - i - 1 # checked
+            op_ij = off_diag_operators[flat_index]
+            off_diag_eval = off_diag_evals[flat_index]
+            eval_scores[i, j] = np.linalg.norm(op_ij @ ref_state - off_diag_eval * ref_state)**2
+
+    return eval_scores
 
 # Template to design alternative cost functions:
 
--- dam/utils.py
+++ dam/utils.py
@@ -5,35 +5,6 @@ import math
 import cmath
 from scipy.special import factorial
 
-# compare with show_symmetries.py, lines 44-62
-def get_heatmap_data(h, ref_state, norb, diag_operators, off_diag_operators, upscale_factor=1):
-    """Returns matrix of nc_scores, with entries ||[H, quasisymmetry_operator]|Ψ>||²"""
-    # Initialize heatmap matrix
-    nc_scores = np.zeros((norb, norb))
-
-    # precompute H|psi>
-    h_on_ref_state = h @ ref_state
-
-    # Diagonal: Possibility to scale up with upscale_factor, for better visualization
-    for i in range(norb):
-        term1 = h @ (diag_operators[i] @ ref_state)
-        term2 = diag_operators[i] @ h_on_ref_state # using the wrapper variable type. Commutator has same/related type
-        commutator_on_state = term1 - term2
-        nc_scores[i, i] = upscale_factor * np.linalg.norm(commutator_on_state)**2
-
-    # Off-diagonal
-    # Need to arefully handle indices (e.g., do inverse of index mapping of what is done in cluster_number.build_two_orb_num_operators)
-    for i in range(norb):
-        for j in range(i+1, norb):
-            flat_index = i * norb - i * (i + 1) // 2 + j - i - 1 # checked
-            op_ij = off_diag_operators[flat_index]
-            term1 = h @ (op_ij @ ref_state)
-            term2 = op_ij @ h_on_ref_state
-            commutator_on_state = term1 - term2
-            nc_scores[i, j] = np.linalg.norm(commutator_on_state)**2
-
-    return nc_scores
-
 #compare with show_symmetries.py, lines 64–76
 def show_heatmap(nc_scores, vmin=None, vmax=None, title='Quasisymmetry discovery'):
     """Visualize the non-commutativity heatmap"""
